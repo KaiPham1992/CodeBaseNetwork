24e5ce3039123cff36a511c430851e21c659acad
Kai Pham <kkai@vinova.sg>
2017-12-23 11:05:53 +0700

    done Base API

---
+++ CodeBaseNetWork/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CodeBaseNetWork
+//
+//  Created by Kai Pham on 12/22/17.
+//  Copyright © 2017 Kai Pham. All rights reserved.
+//
+
+import UIKit
+import  RxSwift
+
+class ViewController: UIViewController {
+    let bag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getOne()
+    }
+
+    
+    func getList() {
+        APIProvider(target: TestAPI.getAllWordbook()).rxRequestArray(TestModel.self).subscribe(onNext: { (testList) in
+            testList.forEach({ (test) in
+                print(test.nameWordBook)
+            })
+        }, onError: { (error) in
+            print(error.localizedDescription)
+        }).disposed(by: bag)
+    }
+    
+    func getOne() {
+        APIProvider(target: TestAPI.getWordbookWith(id: "Wb2")).rxRequestObject(TestModel.self).subscribe(onNext: { (model) in
+            print(model?.nameWordBook)
+        }, onError: { (error) in
+            print(error.localizedDescription)
+        }).disposed(by: bag)
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+
+target 'CodeBaseNetWork' do
+  use_frameworks!
+  
+  #networking
+	pod 'RxSwift'
+	pod 'RxCocoa'
+	pod 'ObjectMapper'
+    pod 'SwiftyJSON'
+
+end
